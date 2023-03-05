5429778d8ec79bbe6aa3351562290acfe93dcf1b
Nandafrdika <nandafredika6@gmail.com>
2023-03-05 12:38:32 +0700

    feat: membuat Api register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function registerUser(request $request)
+    {
+        try {
+
+            $validated = $this->validate($request, [
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:6',
+                'alamat' => 'required|string'
+            ]);
+
+            $validated['password'] = bcrypt($validated['password']);
+            $user = User::create($validated);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'User registered successfully',
+                'user' => $user,
+                'access_token' => $token
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'validation Exception',
+                'error' => $e->errors()
+            ], 422);
+        } 
+        
+        
+        catch (\Exception $e) {
+            return response()->json([
+                'message' => 'failed to register user',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}

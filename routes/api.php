1436912e358557a891b7b790d700ccc551f58f5f
kdsetiawan <kadeksetiawan50@gmail.com>
2023-03-05 13:40:31 +0800

    membuat register

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,7 @@ use App\Http\Controllers\BarangController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/users', [AuthController::class, 'registerUser']);
 
 Route::get('/barang', [BarangController::class, 'index']);

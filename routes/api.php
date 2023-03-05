e4d98e89edb9033b09810e579abab8a3e1c6e63f
okadarma23 <gedeokadarmaputra.shaft@gmail.com>
2023-03-05 13:40:28 +0800

    phase register

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use app\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,5 +20,7 @@ use app\Http\Controllers\BarangController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/users', [AuthController::class, 'registerUser']);
 
 Route::get('/barang',[BarangController::class, 'index']);

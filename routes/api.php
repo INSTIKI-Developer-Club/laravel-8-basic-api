6f8187d6ac4f078f1012c22683cfd508178c5f23
dksurajati <dekasurajati@gmail.com>
2023-03-05 13:40:46 +0800

    membuat API register User

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/users', [AuthController::class, 'registerUser']);
 Route::get('/barang', [BarangController::class, 'index']);
